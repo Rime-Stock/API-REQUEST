d078a00ea1b1294c24b0d390f37922621fba25f9
Shane Ericksen <s@ericksen.me>
2022-10-20 14:16:54 +1000

    Initial Commit

---
+++ API-REQUEST/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  API-REQUEST
+//
+//  Created by Shane Ericksen on 20/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
